52e42940081f15ff6da0ef4a73e2c93b4e4f9ddd
Marcelo Vallejos <128014986+Valls017@users.noreply.github.com>
2024-10-29 13:01:30 -0400

    Add files via upload

---
+++ Calculadora_Software educativo de matematicas para primaria/Calculator.java
@@ -0,0 +1,64 @@
+public class Calculator {
+    private int upperLimit;
+    private int lowerLimit;
+    
+    public Calculator(){}
+    
+    public Calculator(int minValue, int maxValue) {
+        upperLimit = maxValue;
+        lowerLimit = minValue;
+    }
+    
+    public int getLowerLimit() {
+        return lowerLimit;
+    }
+    
+    public void setLowerLimit(int minValue) {
+        lowerLimit = minValue;
+    }
+    
+    public int getUpperLimit() {
+        return upperLimit;
+    }
+    
+    public void setUpperLimit(int maxValue) {
+        upperLimit = maxValue;
+    }
+    
+    public int add(int arg1, int arg2) {
+        validateArgs(arg1, arg2);
+        int result = arg1 + arg2;
+        if(result > upperLimit) {
+            throw new ArithmeticException(
+            "Upper limit exceeded");
+        }
+        return result;
+    }
+    
+    public int substract(int arg1, int arg2) {
+        validateArgs(arg1, arg2);
+        int result = arg1 - arg2;
+        if(result > upperLimit) {
+            throw new ArithmeticException(
+            "Lower limit exceeded");
+        }
+        return result;
+    }
+    
+    public boolean validateArgs(int arg1, int arg2) {
+        if(arg1 > upperLimit) {
+            throw new ArithmeticException(
+            "First argument exceeded upper limit");
+        }else if(arg2 < lowerLimit) {
+            throw new ArithmeticException(
+            "Second argument exceeds lower limit");
+        }else if(arg1 < lowerLimit) {
+            throw new ArithmeticException(
+            "First argument exceeds lower limit");
+        }else if(arg2 > upperLimit) {
+            throw new ArithmeticException(
+            "Second argument exceeds upper limit");
+        }
+        return true;
+    }
+}
---
+++ Calculadora_Software educativo de matematicas para primaria/CalculatorTest.java
@@ -0,0 +1,120 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+    private Calculator calculator;
+    @BeforeEach
+    public void setUp() {
+        calculator = new Calculator();
+    }
+    
+    @Test
+    public void add() {
+        calculator.setUpperLimit(10);
+        int result = calculator.add(2, 2);
+        assertEquals(4, result);
+    }
+    
+    @Test
+    public void addWithDifferentArguments() {
+        calculator.setUpperLimit(10);
+        int result = calculator.add(2, 5);
+        assertEquals(7, result);
+    }
+    
+    @Test
+    public void substract() {
+        calculator.setUpperLimit(10);
+        calculator.setLowerLimit(-10);
+        int result = calculator.substract(5, 3);
+        assertEquals(2, result);
+    }
+    
+    @Test
+    public void substractReturningNegative() {
+        calculator.setUpperLimit(10);
+        calculator.setLowerLimit(-10);
+        int result = calculator.substract(3, 5);
+        assertEquals(-2, result);
+    }
+    
+    @Test
+    public void substractSettingLimintValues() {
+        Calculator calculator = new Calculator(-100, 100);
+        int result = calculator.substract(5, 10);
+        assertEquals(-5, result);
+    }
+    
+    @Test
+    public void substractExcedingLowerLimit() {
+        Calculator calculator = new Calculator(-100, 100);
+        try{
+            int result = calculator.substract(10,150);
+            fail("Exception is not being thrown when exceeding lower limit");
+        }catch(ArithmeticException e){
+            // Ok, the SUT works as expected
+        }
+    }
+    
+    @Test
+    public void setAndGetUpperLimt() {
+        Calculator calculator = new Calculator(-100, 100);
+        assertEquals(100, calculator.getUpperLimit());
+    }
+    
+    @Test
+    public void setAndGetLimit() {
+        Calculator calculator = new Calculator(-100, 100);
+        assertEquals(100, calculator.getUpperLimit());
+        assertEquals(-100, calculator.getLowerLimit());
+    }
+    
+    @Test
+    public void addExcedingUpperLimit() {
+        Calculator calculator = new Calculator(-100, 100);
+        try{
+            int result = calculator.add(10, 150);
+            fail("This should fail: we're exceding upper limit");
+        }catch(ArithmeticException e) {
+            //Ok, the SUT works as expected
+        }
+    }
+    
+    @Test
+    public void argumentsExceedLimits() {
+        Calculator calculator = new Calculator(-100, 100);
+        try{
+            calculator.validateArgs(
+                calculator.getUpperLimit() +1, calculator.getLowerLimit() -1);
+            fail("This should fail: arguments exceed limits");
+        }catch(ArithmeticException e) {
+            //OK, this works
+        }
+    }
+    
+    @Test
+    public void argumentsExceedLimitsInverse() {
+        Calculator calculator = new Calculator(-100, 100);
+        try{
+            calculator.validateArgs(
+                calculator.getLowerLimit() -1, calculator.getUpperLimit() +1);
+            fail("This should fail: arguments exceed limits");
+        }catch(ArithmeticException e) {
+            // Ok, this works
+        }
+    }
+    
+    @Test
+    public void argumentsExceedLimitsOnSubstract() {
+        Calculator calculator = new Calculator(-100, 100);
+        try{
+            calculator.substract(
+                calculator.getUpperLimit() +1, calculator.getLowerLimit() -1);
+            fail("This should fail: arguments exceed limits");
+        }catch(ArithmeticException e) {
+            //Ok, this works
+        }
+    }
+}
